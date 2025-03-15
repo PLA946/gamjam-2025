c4debca684644d02dc9464c7933be32db99d4ff6
Baxoner <baxoner5050@gmail.com>
2025-03-16 00:36:27 +0100

    somewhat getting closer to getting the waves managed

--- scenes/entities/enemies/enemy_1/enemy_1.gd
+++ scenes/entities/enemies/enemy_1/enemy_1.gd
@@ -1,1 +1,4 @@
 extends Entity
+
+func _process(delta: float) -> void:
+	(get_parent() as PathFollow2D).progress = (get_parent().progress+(speed*delta))
--- scenes/map/level.gd
+++ scenes/map/level.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+signal wave_start(num: int)
 
 func _ready() -> void:
 	var scene = load("res://scenes/ui/overlay/overlay.tscn")
 	var overlay = scene.instantiate()
 	$Overlay.add_child(overlay) 
 	overlay.update_data(get_name())
+	wave_start.emit(0)
